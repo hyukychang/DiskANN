ac6b60172b84f4f8caff3fdb3d8fe5e0df12b040
hyukychang <seunghyuk.dev@gmail.com>
2024-08-22 15:52:58 +0900

    add split method

--- apps/hello_mpi.cpp
+++ apps/hello_mpi.cpp
@@ -30,6 +30,22 @@
 #define HYUK_DEBUG false
 #define MASTER_RANK 0
 
+#include <sstream>
+
+std::vector<std::string> split(const std::string &str, char delimiter)
+{
+    std::vector<std::string> tokens;
+    std::string token;
+    std::stringstream ss(str);
+
+    while (std::getline(ss, token, delimiter))
+    {
+        tokens.push_back(token);
+    }
+
+    return tokens;
+}
+
 namespace po = boost::program_options;
 
 /*
@@ -326,10 +342,10 @@ int search_memory_index_with_id_map(diskann::Metric &metric, const std::string &
                 std::vector<std::tuple<int, float>> merged_result_vector;
                 for (int i = 0; i < size; i++)
                 {
-                    std::vector<std::string> result_vector = split(received_results[i], '|');
+                    std::vector<std::string> result_vector = std::split(received_results[i], '|');
                     for (int j = 0; j < result_vector.size(); j++)
                     {
-                        std::vector<std::string> id_dist = split(result_vector[j], ',');
+                        std::vector<std::string> id_dist = std::split(result_vector[j], ',');
                         merged_result_vector.push_back(std::make_tuple(std::stoi(id_dist[0]), std::stof(id_dist[1])));
                     }
                 }
